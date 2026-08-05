93dcfed986d49ff52f98558c0b7b77dc404a9e1b
sevenfifty777 <sevenfifty777@users.noreply.github.com>
2026-08-05 09:34:04 +0000

    deploy: 0eef833a94924fa32b84ae5111a767d2d6fad667

---
+++ js/extra.js
@@ -0,0 +1,79 @@
+$.fn.onClassChange = function (cb) {
+  return $(this).each((_, el) => {
+    new MutationObserver((mutations) => {
+      mutations.forEach(
+        (mutation) => cb && cb(mutation.target, mutation.target.className),
+      );
+    }).observe(el, {
+      attributes: true,
+      attributeFilter: ["class"], // only listen for class attribute changes
+    });
+  });
+};
+
+// Disabled for now, because it causes page-changes when clicking the chapter collapse/expand button as well
+/*
+$("li.chapter-item").on("click", function (e) {
+  // When clicking next to the anchor in the nav bar, click the anchor as well
+  e.stopPropagation();
+  $(this).find("a")[0].click();
+});
+*/
+
+$("div.menu-logo img, h1.menu-title").on("click", function (e) {
+  // When clicking logo or title text, click main chapter link
+  e.stopPropagation();
+  $("ol.chapter").children(":first").click();
+});
+
+function swapLightDarkModeImages() {
+  $("img").each(function () {
+    const e = $(this);
+    const src = e.attr("src");
+    const useDark = $("html").hasClass("hb_dark");
+
+    if (!src) {
+      return;
+    }
+
+    if (useDark && src.includes("img/light/")) {
+      e.attr("src", src.replace("img/light/", "img/dark/"));
+    }
+
+    if (!useDark && src.includes("img/dark/")) {
+      e.attr("src", src.replace("img/dark/", "img/light/"));
+    }
+  });
+}
+
+function scrollToUrlAnchor() {
+  if (!location.hash) {
+	return;
+  }
+
+  const id = decodeURIComponent(location.hash.slice(1));
+  const el = document.getElementById(id) || document.getElementsByName(id)[0];
+
+  if (el) {
+	el.scrollIntoView();
+  }
+}
+
+$(document).ready(function () {
+  swapLightDarkModeImages();
+  scrollToUrlAnchor();
+});
+
+$("html").onClassChange((el, newClass) => {
+  swapLightDarkModeImages();
+});
+
+window.setTheme = function setTheme(theme_id) {
+  $("button.theme#mdbook-theme-" + theme_id).click();
+};
+
+window.enableGameMode = function enableGameMode() {
+  $("#mdbook-theme-toggle").hide(); // Themes switch automatically based on in-game time
+  $(".right-buttons").hide(); // Print, PDF, GitHub, Edit Buttons
+  $(".menu-logo").hide(); // Logo on the sidebar uses absolute path and hence is broken in-game
+};
